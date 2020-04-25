3bce6b8ab0a9dd64b1b5dd1e52e2280136ffbce5
Abe <abhi.rawat456@gmail.com>
2020-04-25 10:45:57 +0530

    Added parser

--- parser.js
+++ parser.js
@@ -1,1 +1,18 @@
-"priceblock_ourprice";
+const nightmare = require("nightmare")();
+
+checkPrice();
+
+async function checkPrice() {
+  const priceString = await nightmare
+    .goto("https://www.amazon.in/dp/B01GGKYS6E")
+    .wait("#priceblock_ourprice")
+    .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+    .end();
+
+  const priceNumber = parseFloat(priceString.replace("₹", ""));
+  if (priceNumber < 700) {
+    console.log("It is cheap");
+  } else {
+    console.log("Expensive");
+  }
+}
